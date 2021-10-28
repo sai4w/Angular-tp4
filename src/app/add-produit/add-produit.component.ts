41938fda57bd0ea14c278ffac512ad589f399bea
saif zhioua <seif.zhioua@gmail.com>
2021-10-28 23:58:50 +0100

    Saif

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+  styleUrls: ['./add-produit.component.css']
+})
+export class AddProduitComponent implements OnInit {
+
+  newProduit = new Produit();
+
+  msg : String;
+
+  constructor(private produitService: ProduitService ) {
+
+  }
+
+  ngOnInit(): void {
+  }
+  addProduit(){
+    this.produitService.ajouterProduit(this.newProduit);
+    
+   
+   
+  }
+
+}
---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent implements OnInit {
+  produits : Produit[]; 
+
+  constructor(private produitService: ProduitService ) {
+    this.produits = produitService.listeProduits();
+
+  }
+
+  ngOnInit(): void {
+  }
+  supprimerProduit(prod:Produit)
+  {
+    let conf = confirm("Etes-vous sûr ?");
+    if (conf)
+    this.produitService.supprimerProduit(prod);
+  }
+
+}
